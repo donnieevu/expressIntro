09f9cd3a145085ca5c7e3665c81fb96f6b112240
Donald Vu <donaldhdvu@gmail.com>
2020-12-09 14:13:25 -0600

    including bootstrap with express

--- templatingPractice/index.js
+++ templatingPractice/index.js
@@ -3,21 +3,22 @@ const app = express();
 const path = require('path')
 const redditData = require('./data.json')
 
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, '/views'))
+app.set('views', path.join(__dirname, '/views'));
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home');
 })
 
 app.get('/r/:subreddit', (req, res) => {
-    const { subreddit } = req.params
-    const data = redditData[subreddit]
+    const { subreddit } = req.params;
+    const data = redditData[subreddit];
     if (data) {
-        res.render('subreddit', { ...data })
+        res.render('subreddit', { ...data });
     } else {
-        res.render('notfound', { subreddit })
+        res.render('notfound', { subreddit });
     }
 
 })
@@ -25,13 +26,13 @@ app.get('/r/:subreddit', (req, res) => {
 app.get('/anime', (req, res) => {
     const animes = [
         'MHA', 'Full-Metal-Alchemist', 'Jujutsu-Kaisen', 'DBZ', 'AOT'
-    ]
-    res.render('anime', { animes })
+    ];
+    res.render('anime', { animes });
 })
 
 app.get('/rand', (req, res) => {
-    const num = Math.floor(Math.random() * 10) + 1
-    res.render('random', { num })
+    const num = Math.floor(Math.random() * 10) + 1;
+    res.render('random', { num });
 })
 
 app.listen(8081, () => {
